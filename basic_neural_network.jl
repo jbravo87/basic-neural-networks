91a763eacec6d8f5e69d075a6d368f9d0bc4b936
Joe Bravo <joebravo@Joes-MacBook-Air.local>
2023-10-11 23:30:02 -0700

    Added matrix multiplication and activation function

--- basic_neural_network.jl
+++ basic_neural_network.jl
@@ -2,14 +2,15 @@
 #=
 A simple 2-layer neural network using structs.
 =#
+#using LinearAlgebra # <- Don't need this function.
 
 # Input, output, output layer, and weights
 struct InitialConditions
-    input::Array{Float64}
-    weights_1::Array{Float64}
-    weights_2::Array{Float64}
-    y::Array{Float64}
-    output::Array{Float64}
+    input::Matrix{Float64}
+    weights_1::Matrix{Float64}
+    weights_2::Matrix{Float64}
+    y::Matrix{Float64}
+    output::Matrix{Float64}
 end
 
 # Input
@@ -20,18 +21,26 @@ y = [0 1 1 0]
 weights_1 = rand(size(x)[2], 4)
 output = zeros(size(y))
 
+# Test dot product matrix operation
+function dot_product(self::InitialConditions)
+    return self.input * self.weights_1
+end
+
 # Instance of InitialConditions
-initial_conditions = InitialConditions(x, y, weights_1, rand(4, 1), output)
+initial_conditions = InitialConditions(x, weights_1, rand(4, 1), y, output)
 #initial_conditions.output
+
+matrix_1 = dot_product(initial_conditions)
+
+# Activation function
+function sigmoid(t)
+    return 1/(1 + exp(-t))
+end
+
 
 # Feedforward
 function feedforward(self::InitialConditions)
 end
 
 # Backpropogation
-
-
-
-
-
 # End of basic_neural_network.jl
