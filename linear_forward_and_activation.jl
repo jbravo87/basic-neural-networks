df91f4feb1e3d1cd72959678da7df8b5e03d91e6
Joe Bravo <joepbravo@gmail.com>
2023-11-07 22:17:57 -0800

    Packaged linear forward logic into one function

--- linear_forward_and_activation.jl
+++ linear_forward_and_activation.jl
@@ -2,7 +2,7 @@
 #=
 Moving data through the network
 First, linear output
-Linear forward computation
+Create a linear forward computation
 =#
 # testing some linear algebra calculations
 # 2x3 matrix
@@ -15,8 +15,26 @@ z = ( w * a ) .+ b
 print(z)
 print("\nsize of matrix z: ", size(z))
 print("\nnumber of rows in matrix w: ", size(w, 1))
-print("\nnumber of columns in matrx a: ", size(a, 2))
+println("\nnumber of columns in matrix a: ", size(a, 2))
+@assert size(z) == (size(w,1), size(a,2)) "\nFinal matrix size doesn't match."
+# Above into a function
+"""
+Create linear forward and return
+inputs as cache. 
 
-@assert size(z) == (size(w,1), size(a,1))
+## Input
+
+- `a` -- input layer 
+- `w` -- weights of the layer
+- `b` -- biases of the layer
+"""
+function linear_forward(a, w, b,)
+    z = ( w * a ) .+ b
+    cache = (a, w, b)
+
+    @assert size(z) == (size(w,1), size(a,2)) "\nFinal matrix size doesn't match."
+
+    return (z, cache)
+end
 
 # End of "linear_forward_and_activation.jl"
